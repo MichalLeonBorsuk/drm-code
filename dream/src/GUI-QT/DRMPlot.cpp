0b45ce7829e3610caec1df03aae4192bf7767766
dflamand <dflamand@ddb839e9-6489-4ed3-a1ee-f9006aeb25b6>
2012-10-30 23:30:50 +0000

    grid size changed

--- dream/src/GUI-QT/DRMPlot.cpp
+++ dream/src/GUI-QT/DRMPlot.cpp
@@ -511,8 +511,8 @@ void CDRMPlot::SetPlotStyle(const int iNewStyleID)
 	/* Apply colors */
 	grid.setMajPen(QPen(MainGridColorPlot, 0, Qt::DotLine));
 	grid.setMinPen(QPen(MainGridColorPlot, 0, Qt::DotLine));
-	vcurvegrid.setPen(QPen(MainGridColorPlot, 0, Qt::DotLine));
-	hcurvegrid.setPen(QPen(MainGridColorPlot, 0, Qt::DotLine));
+	vcurvegrid.setPen(QPen(MainGridColorPlot, 5, Qt::DotLine));
+	hcurvegrid.setPen(QPen(MainGridColorPlot, 5, Qt::DotLine));
 	plot->setCanvasBackground(QColor(BckgrdColorPlot));
 
 	/* Make sure that plot are being initialized again */
