69cb32126b492a9a31242421293cc419e1550d0c
dflamand <dflamand@ddb839e9-6489-4ed3-a1ee-f9006aeb25b6>
2012-11-16 04:17:24 +0000

    BWSViewer: rewrite, implemented memory cache of received mot objects (writing received files to disk is optional), ContentType now supported, status information (number of object in cache, cache size, object pending), added option to allow external link (default to on), added stop/refresh button, added page title.

--- dream/src/GUI-QT/BWSViewer.h
+++ dream/src/GUI-QT/BWSViewer.h
@@ -1,9 +1,9 @@
 /******************************************************************************\
  * British Broadcasting Corporation
- * Copyright (c) 2009
+ * Copyright (c) 2009, 2012
  *
  * Author(s):
- *	 Julian Cable
+ *	 Julian Cable, David Flamand (rewrite)
  *
  * Description: MOT Broadcast Website Viewer
  *
@@ -34,9 +34,102 @@
 
 #include "../DrmReceiver.h"
 #include "DialogUtil.h"
+#include <QMutex>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QNetworkRequest>
+
 
 class CSettings;
 class CDataDecoder;
+
+
+class CWebsiteObject
+{
+public:
+    CWebsiteObject(unsigned int id, const QString& strContentType, CVector<_BYTE>& vecbData)
+        : id(id), strContentType(strContentType),
+        data(QByteArray((const char*)&vecbData[0], vecbData.Size())) {}
+    virtual ~CWebsiteObject() {}
+    unsigned int    id;
+    QString         strContentType;
+    QByteArray      data;
+};
+
+
+class CWebsiteCache : public QObject
+{
+    Q_OBJECT
+
+public:
+    CWebsiteCache() : id_counter(0), total_size(0) {}
+    virtual ~CWebsiteCache() {}
+    void GetObjectCountAndSize(unsigned int& count, unsigned int& size);
+    void ClearObjects();
+    void AddObject(QString strObjName, QString strContentType, CVector<_BYTE>& vecbData);
+    int GetObjectContentType(const QString& strObjName, QString& strContentType);
+    int GetObjectSize(const QString& strObjName, const unsigned int id);
+    int CopyObjectData(const QString& strObjName, const unsigned int id, char *buffer, int maxsize, int offset);
+
+protected:
+    CWebsiteObject* FindObject(const QString& strObjName);
+    std::map<QString,CWebsiteObject> objects;
+    QMutex          mutex;
+    unsigned int    id_counter;
+    unsigned int    total_size;
+
+signals:
+	void ObjectAdded(QString strObjName);
+};
+
+
+class CNetworkReplyCache : public QNetworkReply
+{
+    Q_OBJECT
+
+public:
+    CNetworkReplyCache(QObject* parent, QNetworkAccessManager::Operation op,
+        const QNetworkRequest& req, CWebsiteCache& cache, volatile int& waitobjs);
+    virtual ~CNetworkReplyCache();
+    void abort() { id = 0; }
+    qint64 bytesAvailable() const;
+    bool isSequential() const { return true; }
+
+protected:
+    qint64 readData(char * data, qint64 maxSize);
+    void customEvent(QEvent* event);
+    QMutex              mutex;
+    CWebsiteCache&      cache;
+    volatile int&       waitobjs;
+    QString             path;
+    int                 readOffset;
+    bool                emitted;
+    unsigned int        id;
+
+public slots:
+    void CheckObject(QString strObjName);
+};
+
+
+class CNetworkAccessManager : public QNetworkAccessManager
+{
+    Q_OBJECT
+
+public:
+    CNetworkAccessManager(QObject* parent, CWebsiteCache& objs,
+        volatile int& waitobjs, const bool& bAllowExternalContent, const QString& strCacheHost)
+        : QNetworkAccessManager(parent), objs(objs), waitobjs(waitobjs),
+        bAllowExternalContent(bAllowExternalContent), strCacheHost(strCacheHost) {};
+    virtual ~CNetworkAccessManager() {};
+
+protected:
+    QNetworkReply *createRequest(Operation op, const QNetworkRequest & req, QIODevice *);
+    CWebsiteCache&  objs;
+    volatile int&   waitobjs;
+    const bool&     bAllowExternalContent;
+    const QString&  strCacheHost;
+};
+
 
 class BWSViewer : public QDialog, Ui_BWSViewer
 {
@@ -47,34 +140,47 @@ public:
     virtual ~BWSViewer();
 
 protected:
+    CNetworkAccessManager nam;
     QTimer          Timer;
     CDRMReceiver&   receiver;
     CSettings&      settings;
     QString         strCurrentSavePath;
     QString         strHomeUrl;
     CDataDecoder*   decoder;
-    bool            initialised;
+    CWebsiteCache   wobjs;
+    bool            bHomeSet;
+    bool            bPageLoading;
+    bool            bSaveFileToDisk;
+    bool            bRestrictedProfile;
+    bool            bAllowExternalContent;
+    volatile int    iAwaitingOjects;
+    int             iLastAwaitingOjects;
+    QString         strCacheHost;
     CEventFilter    ef;
 
     bool Changed();
     void CreateDirectories(const QString& filename);
-    void SaveMOTObject(const CMOTObject& obj, const QString& strFileName);
+    void SaveMOTObject(const QString& strObjName, const CMOTObject& obj);
     bool RemoveDir(const QString &dirName, int level = 0);
     void SetupCurrentSavePath();
     void UpdateButtons();
+    void UpdateStatus();
+    QString ObjectStr(unsigned int count);
 
 public slots:
     void OnTimer();
-    void OnSave();
-    void OnSaveAll();
-    void OnClearAll();
     void OnClearCache();
     void OnHome();
+    void OnStopRefresh();
     void OnBack();
     void OnForward();
-    void onSetProfile(bool);
-    void OnLinkClicked(const QUrl & url);
-    void OnLoadFinished(bool ok);
+    void OnSetProfile(bool);
+    void OnSaveFileToDisk(bool);
+    void OnAllowExternalContent(bool);
+    void OnWebViewLoadStarted();
+    void OnWebViewLoadFinished(bool ok);
+    void OnWebViewTitleChanged(const QString& title);
+    void Update();
     void showEvent(QShowEvent*);
     void hideEvent(QHideEvent*);
 };
