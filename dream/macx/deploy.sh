3265a4f2b9043653087402e2f992e3669d0e0716
jcable <jcable@ddb839e9-6489-4ed3-a1ee-f9006aeb25b6>
2013-04-28 09:51:40 +0000

    improved library dependency control

--- dream/macx/deploy.sh
+++ dream/macx/deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+mkdir -p dream
+rm -rf dream/dream.app
+cp -r dream.app dream
+cd dream
 mkdir dream.app/Contents/Frameworks
 cp /usr/local/lib/libhamlib.2.dylib dream.app/Contents/Frameworks/
 cp /opt/local/lib/libz.1.dylib dream.app/Contents/Frameworks/
@@ -8,6 +12,9 @@ cp /opt/local/lib/libFLAC.8.dylib dream.app/Contents/Frameworks/
 cp /opt/local/lib/libvorbis.0.dylib dream.app/Contents/Frameworks/
 cp /opt/local/lib/libvorbisenc.2.dylib dream.app/Contents/Frameworks/
 cp /opt/local/lib/libogg.0.dylib dream.app/Contents/Frameworks/
+
+macdeployqt dream.app
+
 install_name_tool -change /usr/local/lib/libhamlib.2.dylib @executable_path/../Frameworks/libhamlib.2.dylib dream.app/Contents/MacOS/dream 
 install_name_tool -change /opt/local/lib/libportaudio.2.dylib @executable_path/../Frameworks/libportaudio.2.dylib dream.app/Contents/MacOS/dream 
 install_name_tool -change /opt/local/lib/libsndfile.1.dylib @executable_path/../Frameworks/libsndfile.1.dylib dream.app/Contents/MacOS/dream 
@@ -17,5 +24,8 @@ install_name_tool -change /opt/local/lib/libogg.0.dylib @executable_path/../Fram
 install_name_tool -change /opt/local/lib/libvorbis.0.dylib @executable_path/../Frameworks/libvorbis.0.dylib dream.app/Contents/Frameworks/libsndfile.1.dylib 
 install_name_tool -change /opt/local/lib/libvorbisenc.2.dylib @executable_path/../Frameworks/libvorbisenc.2.dylib dream.app/Contents/Frameworks/libsndfile.1.dylib 
 install_name_tool -change /opt/local/lib/libFLAC.8.dylib @executable_path/../Frameworks/libFLAC.8.dylib dream.app/Contents/Frameworks/libsndfile.1.dylib 
+install_name_tool -change /opt/local/lib/libogg.0.dylib @executable_path/../Frameworks/libogg.0.dylib dream.app/Contents/Frameworks/libvorbisenc.2.dylib 
+install_name_tool -change /opt/local/lib/libvorbis.0.dylibb @executable_path/../Frameworks/libvorbis.0.dylib dream.app/Contents/Frameworks/libvorbisenc.2.dylib 
 
-macdeployqt dream.app -dmg
+cd ..
+hdiutil create dream.dmg -srcfolder dream/ -ov
