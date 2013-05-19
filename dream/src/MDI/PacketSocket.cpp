9fe6bf434468d96bb11566fdda85e4e11f2581c9
Julian Cable <152389+jcable@users.noreply.github.com>
2013-05-19 22:30:25 +0000

    RSCI improvements.

--- dream/src/MDI/PacketSocket.cpp
+++ dream/src/MDI/PacketSocket.cpp
@@ -317,6 +317,8 @@ CPacketSocketNative::SetOrigin(const string & strNewAddr)
         sa.sin_family = AF_INET;
         sa.sin_addr.s_addr = htonl(INADDR_ANY);
         sa.sin_port = htons(port);
+        int optval = 1;
+        setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (char*)&optval, sizeof optval);
         int r = bind(s, (sockaddr*)&sa, sizeof(sa));
         if (r < 0)
         {
