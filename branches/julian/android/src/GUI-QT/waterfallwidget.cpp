bcfdb9932575dfe6f997d65368a4e77693a82fdd
jcable <jcable@ddb839e9-6489-4ed3-a1ee-f9006aeb25b6>
2013-11-03 11:17:29 +0000

    re-engineered WaterfallWidget uses QPainter to scale the pixmap and draws points directly onto the canvas pixmap.
    CDRMPlot sets a layout on the QwtPlotCanvas to get the correct resize
    behaviour;

--- branches/julian/android/src/GUI-QT/waterfallwidget.cpp
+++ branches/julian/android/src/GUI-QT/waterfallwidget.cpp
@@ -1,104 +1,70 @@
 #include "waterfallwidget.h"
 #include <QPainter>
 #include <../matlib/MatlibStdToolbox.h>
-#include "DRMPlot.h"
 
 WaterfallWidget::WaterfallWidget(QWidget *parent) :
-    QWidget(parent),Image(),Canvas()
+    QWidget(parent),Canvas()
 {
     setAttribute(Qt::WA_OpaquePaintEvent, true);
     setAttribute(Qt::WA_TransparentForMouseEvents, true);
-    setCursor(Qt::CrossCursor); show();
+    setCursor(Qt::CrossCursor);
+    setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 }
 
 void WaterfallWidget::paintEvent(QPaintEvent *)
 {
-    QPainter Painter(this);
-    Painter.drawPixmap(0, 0, Canvas);
+    QPainter painter(this);
+    // let the painter scale the data to the window
+    painter.setWindow(Canvas.rect());
+    painter.drawPixmap(0, 0, Canvas);
 }
 
-void WaterfallWidget::resizeEvent(QResizeEvent *e)
+void WaterfallWidget::updatePlot(CVector<_REAL>& vecrData, _REAL min, _REAL max)
 {
-    QPixmap tmpPixmap;
-    /* If only the height has changed, then copy the content
-       to the newly allocated Canvas */
-    if(e->oldSize().height() == e->size().height())
+    /* Init some constants */
+    const int iMaxHue = 359; /* Range of "Hue" is 0-359 */
+    const int iMaxSat = 255; /* Range of saturation is 0-255 */
+
+    if(vecrData.Size() != Canvas.width())
     {
-        tmpPixmap = QPixmap(Canvas);
-    }
+        // size the canvas to the data
+        Canvas = QPixmap(vecrData.Size(), height());
 
-    /* Allocate the new canvas */
-    Canvas = QPixmap(e->size());
-
-
-    const QColor& backgroundColor = palette().color(QPalette::Window);
-    Canvas.fill(backgroundColor);
-
-    Image = QImage(e->size().width(), 1, QImage::Format_RGB32);
-
-    if(e->oldSize().height() == e->size().height())
-    {
-        QPainter p(&Canvas);
-        p.drawPixmap(0, 0, e->size().width(), e->size().height(),
-                     tmpPixmap, 0, 0, e->size().width(), e->size().height());
-    }
-}
-
-void WaterfallWidget::updatePlot(CVector<_REAL>& vecrData)
-{
-    /* Resample input data */
-    CVector<_REAL> *pvecrResampledData;
-    Resample.Resample(&vecrData, &pvecrResampledData, Canvas.size().width(), TRUE);
-
-    /* The scaling factor */
-    const _REAL rScale = _REAL(pvecrResampledData->Size()) /  Canvas.size().width();
-
-    /* Actual waterfall data */
-    QColor color;
-    QRgb *imageData = (QRgb*)Image.bits();
-    for (int i = 0; i <  Canvas.size().width(); i++)
-    {
-        /* Init some constants */
-        const int iMaxHue = 359; /* Range of "Hue" is 0-359 */
-        const int iMaxSat = 255; /* Range of saturation is 0-255 */
-
-        /* Stretch width to entire canvas width */
-        const int iCurIdx =
-            (int) Round(_REAL(i) * rScale);
-
-        /* Translate dB-values in colors */
-        const int iCurCol =
-            (int) Round(((*pvecrResampledData)[iCurIdx] - MIN_VAL_INP_SPEC_Y_AXIS_DB) /
-            (MAX_VAL_INP_SPEC_Y_AXIS_DB - MIN_VAL_INP_SPEC_Y_AXIS_DB) *
-            iMaxHue);
-
-        /* Reverse colors and add some offset (to make it look a bit nicer) */
-        const int iColOffset = 60;
-        int iFinalCol = iMaxHue - iColOffset - iCurCol;
-        if (iFinalCol < 0) /* Prevent from out-of-range */
-            iFinalCol = 0;
-
-        /* Also change saturation to get dark colors when low level */
-        int iCurSat = (int) ((1 - (_REAL) iFinalCol / iMaxHue) * iMaxSat);
-        if (iCurSat < 0) /* Prevent from out-of-range */
-            iCurSat = 0;
-
-        /* Generate pixel */
-        color.setHsv(iFinalCol, iCurSat, iCurSat);
-        imageData[i] = color.rgb();
+        // always use a black background
+        Canvas.fill(QColor::fromRgb(0, 0, 0));
     }
 
     /* Scroll Canvas */
-    Canvas.scroll(0, 1, 0, 0, Canvas.size().width(), Canvas.size().height(), 0);
+    Canvas.scroll(0, 1, Canvas.rect());
 
-    /* Paint new line (top line) */
-    QPainter Painter;
-    if (Painter.begin(&Canvas)) {
-        /* Draw pixel line to canvas */
-        Painter.drawImage(0, 0, Image);
+    QPainter painter;
+    painter.begin(&Canvas);
 
-        Painter.end();
+    /* Actual waterfall data */
+    for (int i = 0; i <  vecrData.Size(); i++)
+    {
+        /* Translate dB-values in colors */
+        _REAL norm = (vecrData[i] - min) / (max - min);
+        int hue = int(Round(norm * iMaxHue));
 
-        repaint();
+        /* Reverse colors and add some offset (to make it look a bit nicer) */
+        hue = iMaxHue - 60 - hue;
+        /* Prevent out-of-range */
+        if(hue>iMaxHue)
+            hue = iMaxHue;
+        if (hue < 0)
+            hue = 0;
+
+        /* Also change saturation to get dark colors when low level */
+        int sat = int((1 - double(hue) / iMaxHue) * iMaxSat);
+        if (sat < 0) /* Prevent out-of-range */
+            sat = 0;
+
+        /* Generate pixel */
+        painter.setPen(QColor::fromHsv(hue, sat, sat));
+        painter.drawPoint(i, 0);
     }
+
+    painter.end();
+    repaint();
 }
