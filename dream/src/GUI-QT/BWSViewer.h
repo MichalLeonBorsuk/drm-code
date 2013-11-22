7948723c39e71aef6abca6bbfdaa0933cacc6292
jcable <jcable@ddb839e9-6489-4ed3-a1ee-f9006aeb25b6>
2013-11-22 07:41:51 +0000

    move helper classes from BWSViewer to BWSViewerWidget.

--- dream/src/GUI-QT/BWSViewer.h
+++ dream/src/GUI-QT/BWSViewer.h
@@ -32,107 +32,12 @@
 #include "ui_BWSViewer.h"
 #include "DialogUtil.h"
 #include "CWindow.h"
-#include "../util/Utilities.h"
+#include "bwsviewerwidget.h"
 #include <QMutex>
-#include <QNetworkAccessManager>
-#include <QNetworkReply>
-#include <QNetworkRequest>
-#include <string>
-
 
 class CMOTObject;
 class CDRMReceiver;
 class CDataDecoder;
-
-
-class CWebsiteObject
-{
-public:
-    CWebsiteObject(unsigned int id, const QString& strContentType, CVector<_BYTE>& vecbData)
-        : id(id), strContentType(strContentType),
-        data(QByteArray((const char*)&vecbData[0], vecbData.Size())) {}
-    virtual ~CWebsiteObject() {}
-    unsigned int    id;
-    QString         strContentType;
-    QByteArray      data;
-};
-
-
-class CWebsiteCache : public QObject
-{
-    Q_OBJECT
-
-public:
-    CWebsiteCache() : id_counter(0), total_size(0) {}
-    virtual ~CWebsiteCache() {}
-    void GetObjectCountAndSize(unsigned int& count, unsigned int& size);
-    void ClearAll();
-    void AddObject(QString strObjName, QString strContentType, CVector<_BYTE>& vecbData);
-    int GetObjectContentType(const QString& strObjName, QString& strContentType);
-    int GetObjectSize(const QString& strObjName, const unsigned int id);
-    int CopyObjectData(const QString& strObjName, const unsigned int id, char *buffer, int maxsize, int offset);
-    bool SetDirectoryIndex(const QString strNewDirectoryIndex);
-    QString GetDirectoryIndex();
-
-protected:
-    CWebsiteObject* FindObject(const QString& strObjName);
-    std::map<QString,CWebsiteObject> objects;
-    QMutex          mutex;
-    QString         strDirectoryIndex;
-    unsigned int    id_counter;
-    unsigned int    total_size;
-
-signals:
-	void ObjectAdded(QString strObjName);
-};
-
-
-class CNetworkReplyCache : public QNetworkReply
-{
-    Q_OBJECT
-
-public:
-    CNetworkReplyCache(QNetworkAccessManager::Operation op,
-        const QNetworkRequest& req, CWebsiteCache& cache, CCounter& waitobjs);
-    virtual ~CNetworkReplyCache();
-    void abort() { id = 0; }
-    qint64 bytesAvailable() const;
-    bool isSequential() const { return true; }
-
-protected:
-    qint64 readData(char * data, qint64 maxSize);
-    void customEvent(QEvent* event);
-    CWebsiteCache&      cache;
-    CCounter&           waitobjs;
-    QString             path;
-    int                 readOffset;
-    bool                emitted;
-    unsigned int        id;
-
-public slots:
-    void CheckObject(QString strObjName);
-};
-
-
-class CNetworkAccessManager : public QNetworkAccessManager
-{
-    Q_OBJECT
-
-public:
-    CNetworkAccessManager(QObject* parent, CWebsiteCache& cache,
-        CCounter& waitobjs, const bool& bAllowExternalContent, const QString& strCacheHost)
-        : QNetworkAccessManager(parent), cache(cache), waitobjs(waitobjs),
-        bAllowExternalContent(bAllowExternalContent), strCacheHost(strCacheHost) {};
-    virtual ~CNetworkAccessManager() {};
-
-protected:
-    QNetworkReply *createRequest(Operation op, const QNetworkRequest & req, QIODevice *);
-    CWebsiteCache&  cache;
-    CCounter&       waitobjs;
-    const bool&     bAllowExternalContent;
-    const QString&  strCacheHost;
-};
-
 
 class BWSViewer : public CWindow, public Ui_BWSViewer
 {
