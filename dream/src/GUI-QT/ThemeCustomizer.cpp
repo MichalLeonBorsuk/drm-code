5e308b1ebfdee54bcb11ee19472637116b31f016
dflamand <dflamand@ddb839e9-6489-4ed3-a1ee-f9006aeb25b6>
2013-12-07 21:42:49 +0000

    improved dark theme

--- dream/src/GUI-QT/ThemeCustomizer.cpp
+++ dream/src/GUI-QT/ThemeCustomizer.cpp
@@ -29,9 +29,9 @@
 
 #ifdef USE_THEMECUSTOMIZER
 
-#include <QWidget>
-#include <QFrame>
 #include <QPalette>
+#include <QFont>
+#include <QFrame>
 #include "ui_DRMMainWindow.h"
 #include "ui_serviceselector.h"
 
@@ -46,22 +46,54 @@ void ApplyCustomTheme(QWidget *widget, void* pUi)
 		palette.setColor(QPalette::WindowText, QColor("#FFFFFF"));
 		palette.setColor(QPalette::Button,     QColor("#212421"));
 		palette.setColor(QPalette::ButtonText, QColor("#FFFFFF"));
+		palette.setColor(QPalette::Disabled, QPalette::ButtonText, QColor("#616361"));
 		widget->setPalette(palette);
+		ui->FrameMainDisplay->layout()->setMargin(0);
 		ui->FrameMainDisplay->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+		ui->TextTextMessage->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Ignored);
+		palette = ui->TextTextMessage->palette();
+		palette.setColor(QPalette::Disabled, QPalette::Light, QColor("#616361"));
+		palette.setColor(QPalette::Disabled, QPalette::Dark,  QColor("#616361"));
+		ui->TextTextMessage->setPalette(palette);
+		QFont font(ui->ProgrInputLevel->font());
+		font.setPointSize(font.pointSize()-1);
+		ui->ProgrInputLevel->setFont(font);
+		ui->onebar->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Preferred);
+		ui->onebar->setMinimumSize(0, 6);
+		ui->onebar->setMaximumSize(16777215, 6);
+		ui->twobars->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Preferred);
+		ui->twobars->setMinimumSize(0, 6);
+		ui->twobars->setMaximumSize(16777215, 12);
+		ui->threebars->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Preferred);
+		ui->threebars->setMinimumSize(0, 6);
+		ui->threebars->setMaximumSize(16777215, 18);
+		ui->fourbars->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Preferred);
+		ui->fourbars->setMinimumSize(0, 6);
+		ui->fourbars->setMaximumSize(16777215, 24);
+		ui->fivebars->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Preferred);
+		ui->fivebars->setMinimumSize(0, 6);
+		ui->fivebars->setMaximumSize(16777215, 30);
 	}
 	else if (name == "ServiceSelector")
 	{
 		Ui::ServiceSelector* ui = (Ui::ServiceSelector*)pUi;
-		ui->TextMiniService1->setFrameShape(QFrame::StyledPanel);
-		ui->TextMiniService1->setFrameShadow(QFrame::Plain);
-		ui->TextMiniService2->setFrameShape(QFrame::StyledPanel);
-		ui->TextMiniService2->setFrameShadow(QFrame::Plain);
-		ui->TextMiniService3->setFrameShape(QFrame::StyledPanel);
-		ui->TextMiniService3->setFrameShadow(QFrame::Plain);
-		ui->TextMiniService4->setFrameShape(QFrame::StyledPanel);
-		ui->TextMiniService4->setFrameShadow(QFrame::Plain);
+		QPalette palette(widget->palette());
+		palette.setColor(QPalette::Light, QColor("#616361"));
+		palette.setColor(QPalette::Dark,  QColor("#616361"));
+		ui->TextMiniService1->setFrameShape(QFrame::Panel);
+		ui->TextMiniService1->setFrameShadow(QFrame::Sunken);
+		ui->TextMiniService1->setPalette(palette);
+		ui->TextMiniService2->setFrameShape(QFrame::Panel);
+		ui->TextMiniService2->setFrameShadow(QFrame::Sunken);
+		ui->TextMiniService2->setPalette(palette);
+		ui->TextMiniService3->setFrameShape(QFrame::Panel);
+		ui->TextMiniService3->setFrameShadow(QFrame::Sunken);
+		ui->TextMiniService3->setPalette(palette);
+		ui->TextMiniService4->setFrameShape(QFrame::Panel);
+		ui->TextMiniService4->setFrameShadow(QFrame::Sunken);
+		ui->TextMiniService4->setPalette(palette);
+		widget->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum);
 	}
 }
 
 #endif
-
