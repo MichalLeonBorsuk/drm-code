209c46c956c21e1442d658e8fd455ae2a4ff2dc1
Julian Cable <152389+jcable@users.noreply.github.com>
2014-12-16 20:10:51 +0000

    fix some problems

--- dream/src/sync/SyncUsingPil.cpp
+++ dream/src/sync/SyncUsingPil.cpp
@@ -27,7 +27,7 @@
 \******************************************************************************/
 
 #include "SyncUsingPil.h"
-
+#include <QDebug>
 
 /* Implementation *************************************************************/
 void CSyncUsingPil::ProcessDataInternal(CParameter& Parameters)
@@ -298,6 +298,7 @@ void CSyncUsingPil::ProcessDataInternal(CParameter& Parameters)
     }
 
     Parameters.Unlock();
+    qDebug() << "SyncUsingPil " << iOutputBlockSize;
 }
 
 void CSyncUsingPil::InitInternal(CParameter& Parameters)
