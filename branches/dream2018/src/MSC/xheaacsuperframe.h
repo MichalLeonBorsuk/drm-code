5ab4bac7d32fb8b1decfd5343079478c38f00251
jcable <jcable@ddb839e9-6489-4ed3-a1ee-f9006aeb25b6>
2019-01-13 23:28:07 +0000

    calculate bitResLevel.

--- branches/dream2018/src/MSC/xheaacsuperframe.h
+++ branches/dream2018/src/MSC/xheaacsuperframe.h
@@ -8,11 +8,12 @@ class XHEAACSuperFrame: public AudioSuperFrame
 {
 public:
     XHEAACSuperFrame();
-    void init(unsigned frameSize) { this->frameSize = frameSize; }
+    void init(const CAudioParam& audioParam, unsigned frameSize);
     virtual bool parse(CVectorEx<_BINARY>& asf);
     virtual unsigned getNumFrames() { return audioFrame.size(); }
     virtual void getFrame(std::vector<uint8_t>& frame, uint8_t& crc, unsigned i) { }
 private:
+    unsigned numChannels;
     unsigned frameSize;
     uint8_t previous[2];
 };
