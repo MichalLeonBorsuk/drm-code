f9bb8a9c4a32f5b6342451c10e241316e407d894
Julian Cable <152389+jcable@users.noreply.github.com>
2019-03-24 20:31:16 +0000

    missing

---
+++ branches/dream2018/src/GUI-QT/ctrx.h
@@ -0,0 +1,27 @@
+#ifndef CTRX_H
+#define CTRX_H
+
+#include <QThread>
+#include "../DrmTransceiver.h"
+#include <vector>
+
+enum ERunState { STOPPED, RUNNING, STOP_REQUESTED, RESTART };
+
+class CTRx : public QThread, public CDRMTransceiver
+{
+    Q_OBJECT
+public:
+    explicit CTRx(QThread *parent = nullptr);
+    virtual int GetFrequency()=0;
+
+signals:
+    void InputDeviceChanged(const string&);
+    void OutputDeviceChanged(const string&);
+    void soundUpscaleRatioChanged(int);
+
+public slots:
+    virtual void SetFrequency(int)=0;
+    virtual void SetSoundSignalUpscale(int) {}
+};
+
+#endif // CTRX_H
