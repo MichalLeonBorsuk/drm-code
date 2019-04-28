d5b4d1984b074cd56fc4d08f93119a3e1ca29455
Julian Cable <152389+jcable@users.noreply.github.com>
2019-04-28 18:57:29 +0000

    rename InputPSD class to SpectrumAnalyser.
    Fix bugs preventing display of input spectrum and input PSD.

--- tags/dream-2.2/src/spectrumanalyser.h
+++ tags/dream-2.2/src/spectrumanalyser.h
@@ -5,31 +5,31 @@
 
 class CParameter;
 
-class InputPSD
+class SpectrumAnalyser
 {
 public:
-    InputPSD();
-    void PutPSD(CParameter& Parameters);
+    SpectrumAnalyser();
     void SetSampleRate(int sr) {
         iSampleRate = sr;
     }
     void setNegativeFrequency(bool b) { bNegativeFreq = b; }
     void setOffsetFrequency(bool b) { bOffsetFreq = b; }
+    void CalculateSpectrum(const CShiftRegister<_REAL>& vecrInpData, int n);
     void CalculateLinearPSD(const CShiftRegister<_REAL>& vecrInpData,
                              int iLenPSDAvEachBlock, int iNumAvBlocksPSD,
                              int iPSDOverlap);
-    void PSD2LogPSD(int iLenPSDAvEachBlock, int iNumAvBlocksPSD, CVector<_REAL>& vecrData, CVector<_REAL>& vecrScale);
-
-private:
     void CalculateSigStrengthCorrection(CParameter &Parameters, CVector<_REAL> &vecrPSD);
     void CalculatePSDInterferenceTag(CParameter &Parameters, CVector<_REAL> &vecrPSD);
+    void PSD2LogPSD(_REAL rNormData, CVector<_REAL>& vecrData, CVector<_REAL>& vecrScale);
+
+private:
     _REAL CalcTotalPower(CVector<_REAL> &vecrData, int iStartBin, int iEndBin);
     int FreqToBin(_REAL rFreq);
 
     int iSampleRate;
     bool bNegativeFreq;
     bool bOffsetFreq;
-    CRealVector vecrAvSqMagSpect;
+    CRealVector vecrSqMagSpect;
 };
 
 #endif // INPUTPSD_H
